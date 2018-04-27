31554a0e0ff7c38d4e4093f211b7e8ad04985b86
svlugovoy <svlugovoy@gmail.com>
2018-04-27 11:33:13 +0300

    Refactored using uri builder

--- src/main/java/com/svlugovoy/simplerestclient/restclient/service/ApiServiceImpl.java
+++ src/main/java/com/svlugovoy/simplerestclient/restclient/service/ApiServiceImpl.java
@@ -3,8 +3,10 @@ package com.svlugovoy.simplerestclient.restclient.service;
 import com.svlugovoy.simplerestclient.api.domain.User;
 import com.svlugovoy.simplerestclient.api.domain.UserData;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,21 +15,24 @@ import java.util.stream.Collectors;
 @Slf4j
 public class ApiServiceImpl implements ApiService {
 
-    public static final String URL = "http://apifaketory.com/api/user?limit=";
-
     private RestTemplate restTemplate;
+    private final String api_url;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
 
-        log.info("Invoked getUsers with limit={}", limit);
-        UserData userData = restTemplate.getForObject(URL + limit, UserData.class);
-        log.info("Returned follow users {}",
-                userData.getData().stream().map(User::getEmail).collect(Collectors.toList()));
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .fromUriString(api_url)
+                .queryParam("limit", limit);
+
+        log.info("Invoked getUsers with limit={}, endpoint {}", limit, uriBuilder.toUriString());
+        UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
+        log.info("Returned follow users {}", userData.getData().stream().map(User::getEmail).collect(Collectors.toList()));
 
         return userData.getData();
     }
